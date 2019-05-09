67f6fdf646edb63c8cc2502c9009af8dd21048ab
Ofek Ashery <ofek.ashery@gmail.com>
2019-05-09 13:01:04 +0300

    Update TOA match adapter

--- src/adapters/TOAMatchAdapter.ts
+++ src/adapters/TOAMatchAdapter.ts
@@ -18,9 +18,9 @@ export default class TOAMatchAdapter {
     const keyParams: string[] = matchKey.split("-");
     match.matchKey = this._match.matchKey + "-1";
     match.eventKey = keyParams[0] + "-" + keyParams[1] + "-" + keyParams[2];
-    match.tournamentLevel = this._match.tournamentLevel;
+    match.tournamentLevel = this.getTournamentLevel();
     match.scheduledTime =  moment(this._match.scheduledStartTime.format('YYYY/MM/DD HH:mm:ss')).format("YYYY-MM-DD HH:mm:ss");
-    match.matchName = this._match.matchName;
+    match.matchName = this.getShortName();
     match.playNumber = 1;
     match.fieldNumber = this._match.fieldNumber;
     match.redScore = this._match.redScore;
@@ -36,4 +36,28 @@ export default class TOAMatchAdapter {
     return match;
   }
 
+  getShortName() {
+    const matchName = this._match.matchName;
+    if (matchName.startsWith('Qualification Match')) {
+      return matchName.replace('Qualification Match', 'Quals');
+    } else if (matchName.startsWith('Quarterfinal')) {
+      return matchName.replace('Quarterfinal', 'Quarters');
+    } else if (matchName.startsWith('Semifinal')) {
+      return matchName.replace('Semifinal', 'Semis');
+    } else if (matchName.startsWith('Finals Match')) {
+      return matchName.replace('Finals Match', 'Finals');
+    }
+    return matchName;
+  }
+
+  getTournamentLevel() {
+    const tournamentLevel = this._match.tournamentLevel;
+    if (tournamentLevel >= 20 && tournamentLevel < 40) {
+      return tournamentLevel + 1;
+    } else if (tournamentLevel === 40) {
+      return 4;
+    } else {
+      return tournamentLevel;
+    }
+  }
 }
